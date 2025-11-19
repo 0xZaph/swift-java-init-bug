c336627b8312850a1a18f601f6a75c435194530e
Zaphhh <umarfarrukh2@gmail.com>
2025-11-19 00:08:10 +0000

    Create reproducer

---
+++ Sources/repro/API/Clients/NewsClient.swift
@@ -0,0 +1,16 @@
+//
+//  NewsClient.swift
+//  OnTime
+//
+//  Created by Zaph on 09/08/2025.
+//
+
+import Foundation
+import Observation
+
+@MainActor
+@Observable
+public class NewsClient {
+    public init() {
+    }
+}
