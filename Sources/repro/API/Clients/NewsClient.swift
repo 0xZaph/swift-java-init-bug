0e9f31fe219b63212814dcc96bea9e4e7af5faec
Zaphhh <umarfarrukh2@gmail.com>
2025-11-19 00:19:49 +0000

    More dumbed down repro

--- Sources/repro/API/Clients/NewsClient.swift
+++ Sources/repro/API/Clients/NewsClient.swift
@@ -1,16 +1,4 @@
-//
-//  NewsClient.swift
-//  OnTime
-//
-//  Created by Zaph on 09/08/2025.
-//
-
-import Foundation
-import Observation
-
-@MainActor
-@Observable
-public class NewsClient {
-    public init() {
-    }
+public class Client {
+  public init() {
+  }
 }
